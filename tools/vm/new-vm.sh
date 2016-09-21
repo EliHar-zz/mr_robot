9fb09b8cccf0458551e9af56f5c73ec7cb48b5ec
Amir Bawab <amirbawab@gmail.com>
2016-09-21 09:08:44 -0400

    Create tmp directory if doesn't exist

--- tools/vm/new-vm.sh
+++ tools/vm/new-vm.sh
@@ -71,6 +71,7 @@ sudo apt-get install cmake -y &&
 echo 
 
 confirm "Install OpenCV ?" &&
+[ -e /tmp ] || sudo mkdir /tmp &&
 cd /tmp &&
 sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev -y &&
 sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev -y &&
