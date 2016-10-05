0946e7e1a91a23cabc44200e1a067ba3be117039
Amir Bawab <amirbawab@gmail.com>
2016-10-05 11:09:27 -0400

    Update Q4.c

--- tools/lab/lab_1/Q4.c
+++ tools/lab/lab_1/Q4.c
@@ -13,6 +13,9 @@
 #include <util/delay.h>
 #include "usb_serial.h"
 
+/*
+ * Converts integer to String (char*)
+ */
 char* itoa(int i, char b[]){
 	char const digit[] = "0123456789";
 	char* p = b;
@@ -33,32 +36,55 @@ char* itoa(int i, char b[]){
 	return b;
 }
 
+/*
+ * Setup step:
+ *	- Configure board pins
+ *	- Configure Serial monitor
+ */
 void setup() {
 	DDRB = (1 << PB7); // Set pin PB7 as OUTPUT
 
+	// Fast PWM
 	TCCR0A = (1 << WGM00) | (1 << WGM01) | (1 << COM0A1);
+	
+	// Clock/64 (From prescaler)
 	TCCR0B = (1 << CS00) | (1 << CS01);
 
+	// Reset OCR0A
 	OCR0A = 0;
+
+	// Initialize USB Serial
 	usb_init();
 	sei();
 }
 
+/*
+ * Loop step:
+ * 	- Keep program running
+ *	- Update OCR0A every X ms
+ */
 void loop() {
 	while(1) {
+
+		// Delay the program
 		_delay_ms(100);
-		PORTB |= (1 << PB7);
-		char message[32] = "LED brightness is: ";
-		char brightness[] = "";
-		itoa(OCR0A, brightness);
-		strcat(message, brightness);
-		strcat(message, "\r\n");
-		usb_serial_write(message, 32);
-		OCR0A += 64;
-		PORTB &= ~(1 << PB7);
+
+		// Print the brightness to the Serial Monitor
+		char brightness[50] = "";
+        char integer[20];
+		strcat(brightness, "LED brightness is: ");
+        strcat(brightness, itoa(OCR0A, integer));
+		strcat(brightness, "\n\r");
+		usb_serial_write(brightness, 30);
+
+		// Update OCR0A
+		OCR0A += 20;
 	}
 }
 
+/*
+ * Start program
+ */
 int main() {
 	setup();
 	loop();
