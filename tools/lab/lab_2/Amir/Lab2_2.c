4fb4d15ba38903d5cff4ef9925c9cb444ecdeadb
Amir Bawab <amirbawab@gmail.com>
2016-10-28 12:14:28 -0400

    Added Lab2_2

---
+++ tools/lab/lab_2/Amir/Lab2_2.c
@@ -0,0 +1,38 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+int externalTimer = 0;
+int INT_EVERY = 5; // 5 seconds
+
+// INT0 handler
+ISR(TIMER0_COMPA_vect) {
+
+	// Disable interrupts
+	cli();
+
+	if(externalTimer++ == INT_EVERY * 10) {
+		PORTB ^= (1 << PB1);
+		externalTimer = 0;
+	}
+
+    // Enable interrupts
+    sei();
+}
+
+void setup() {
+    DDRB |= (1 << PB1); // LED on pin PB1
+    TCCR0A = (1 << WGM01); // CTC mode
+    TIMSK0 = (1 << OCIE0A);
+
+    // Value calculated from http://eleccelerator.com/avr-timer-calculator/
+    OCR0A = 97.65625;
+    TCCR0B = (1 << CS00) | (1 << CS02); // 1024 prescalar
+
+	// Enable global interrupts
+	sei();
+}
+
+int main(void) {
+    setup();
+    while(1);
+}
