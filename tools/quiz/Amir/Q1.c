dd6336d52c433261ac8ef4b7147b22fe766582d2
Amir Bawab <amirbawab@gmail.com>
2016-11-18 21:27:32 -0500

    Update Q1.c

--- tools/quiz/Amir/Q1.c
+++ tools/quiz/Amir/Q1.c
@@ -1,4 +1,4 @@
-#define FOSC 1000000                       // Clock Speed
+#define FOSC 1000000
 #define USART_BAUDRATE 4800
 #define BAUD_PRESCALE (((F_CPU/(USART_BAUDRATE*16UL)))-1)
 
@@ -8,88 +8,100 @@
 #include <stdlib.h>
 #include <avr/interrupt.h>
 
-// Global
-int externalTimer_1 = 0;
-int externalTimer_2 = 0;
+// In the following program, variables ending with the number 1
+// correspond to the first LED, and variables ending with the
+// number 2 correspond to the second LED
 
-// Specific
-int delay_1 = 1; // First LED Speed
-int delay_2 = 1; // Second LED Speed
-int intensity_1 = 0; // First LED brightness
-int intensity_2 = 0; // Second LED brightness
+// Timer variables are used to count to a specific unit of time.
+// For example, if g_timer_1 is equal to 1, then the 1 msec has
+// passed. This is useful because we can have accurate timing
+// for blinking the LEDs.
+int g_timer_1 = 0;
+int g_timer_2 = 0;
 
-void toggleLED_1() {
-	if(OCR0A == 0) {
-		OCR0A = intensity_1;
-	} else {
-		OCR0A = 0;
-	}
-}
+// The amount of time specified by the user for blinking an LED.
+// For example, if g_delay_1 is equal to 1, then the first LED is
+// going to blink every 1 second
+int g_delay_1 = 1; 
+int g_delay_2 = 1;
 
-void toggleLED_2() {
-	if(OCR0B == 0) {
-		OCR0B = intensity_2;
-	} else {
-		OCR0B = 0;
-	}
-}
+// The brightness of an LED is also specified by the user.
+// The value for the brightness variables goes from 0 to 255,
+// where 0 is LOW brightness and 255 is HIGH brightness.
+int g_brightness_1 = 0;
+int g_brightness_2 = 0;
 
-// Timer handler
+// Timer 2 handler executes every 1 msec.
+// Each time the handler executes, the timer checks
+// if the first and/or second LED should toggle.
 ISR(TIMER2_COMPA_vect) {
+
+    // Disable interrupts
 	cli();
-	if(externalTimer_1++ >= delay_1) {
-		toggleLED_1();
-		externalTimer_1 = 0;
+
+    // Check if it's time to toggle LED 1
+	if(g_timer_1++ >= g_delay_1) {
+
+        // Toggle LED 1
+        if(OCR0A == 0) {
+            OCR0A = g_brightness_1;
+        } else {
+            OCR0A = 0;
+        }
+		g_timer_1 = 0;
 	}
 
-	if(externalTimer_2++ >= delay_2) {
-		toggleLED_2();
-		externalTimer_2 = 0;
+    // Check if it's time to toggle LED 2
+	if(g_timer_2++ >= g_delay_2) {
+
+        // Toggle LED 2
+        if(OCR0B == 0) {
+            OCR0B = g_brightness_2;
+        } else {
+            OCR0B = 0;
+        }
+		g_timer_2 = 0;
 	}
+
+    // Re-enable interrupts
     sei();
 }
 
-ISR(TIMER2_COMPB_vect) {
+void init_timers() {
+    // Disable interrupts
 	cli();
-    sei();
-}
 
-void init_timer0() {
-	// Enabler for front right wheels, Set as output
+    // Prepare the 2 LEDs on pins PD6 and PD5
 	DDRD |= (1 << PD6);
 	DDRD |= (1 << PD5);
 
+    // Configure the mode of Timer0 which is going to take care
+    // of updating the brightness of the LEDs
 	TCCR0A = (1 << WGM00) | (1 << WGM01) | (1 << COM0A1) | (1 << COM0B1);
 	TCCR0B = (1 << CS00);
-}
 
-void init_timer2() {
-	TCCR2A = (1 << WGM21); // CTC mode
+    // Configure the mode of Timer2 which is goging to take care
+    // of blinking the LEDs according to the values provided
+    // by the user
+	TCCR2A = (1 << WGM21);
 	TIMSK2 = (1 << OCIE2A);
 
 	// Value calculated from http://eleccelerator.com/avr-timer-calculator/
+    // The OCR2A value allows us to "guarantee" an Timer interruption every
+    // 1 msec.
 	OCR2A = 0.9765625;
 	TCCR2B = (1 << CS20) | (1 << CS21) | (1 << CS22); // 1024 prescalar
 
-	// Enable global interrupts
+	// Enable interrupts
 	sei();
 }
 
 void updateLEDs(int d1, int i1, int d2, int i2) {
-	delay_1 = d1;
-	delay_2 = d2;
-	intensity_1 = i1;
-	intensity_2 = i2;
-}
-
-void init_Timers() {
-	cli();
-	init_timer0();
-	init_timer2();
-	sei();
 }
 
 //***************************  USART  *****************************
+// The following code, is a helper code that allow us to log
+// messages and to provide input throght the tty USB
 
 void init_USART() {
 	/*Set baud rate */
@@ -177,8 +189,20 @@ int parseInt(char* integer_str) {
 }
 
 void loop() {
-	while(1) {
+}
 
+int main() {
+
+    // Initialize serial monitor communication
+	init_USART();
+
+    // Initialize timer0 and timer2
+	init_timers();
+
+    // Keep listening for input from the user
+    // Input has the following format:
+    // <First LED delay>;<First LED brightness 0-255>;<Second LED delay>;<Second LED brightness 0-255>;
+	while(1) {
 		int input = 0;
 		int length = 128;
 		char message[length + 1];
@@ -192,32 +216,26 @@ void loop() {
 				int count = 1;
 				tofree = string;
 				int d1 = 0, i1 = 0, d2 = 0, i2 = 0; // variables to hold the speed
-				while ((token = strsep(&string, ",")) != NULL)
+
+                // Specify the delimiter between values to be ';'
+				while ((token = strsep(&string, ";")) != NULL)
 				{
 					input = parseInt(token);
 					switch(count) {
-					case 1: d1 = input;
+					case 1: g_delay_1 = input;
 					break;
-					case 2: i1 = input;
+					case 2: g_brightness_1 = input;
 					break;
-					case 3: d2 = input;
+					case 3: g_delay_2 = input;
 					break;
-					case 4: i2 = input;
+					case 4: g_brightness_2 = input;
 					break;
 					}
 					count++;
-
 				}
-				updateLEDs(d1,i1,d2,i2);
 				free(tofree);
 			}
 		}
 	}
-}
-
-int main() {
-	init_USART();
-	init_Timers();
-	loop();
 	return 0;
 }
