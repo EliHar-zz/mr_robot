90e1d87488d4a90dc5865fbd0af2081e6afd11dc
Amir Bawab <amirbawab@gmail.com>
2016-12-22 11:15:59 -0500

    Update speaker.sh

--- host/cv/speaker.sh
+++ host/cv/speaker.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+print_usage() {
+    cat <<EOF
+Read live car directions from Apache2 server running on the BBB
+
+Usage:
+   $0 <IP>
+EOF
+}
+
+if [[ ${#@} -ne 1 ]]
+then
+    print_usage
+    exit 1
+fi
+
 while true
 do
-    say `curl -s http://192.168.0.150/mr_robot/info.txt | grep Direction | sed 's/Direction: //g'` 
+    say `curl -s http://$1/mr_robot/info.txt | grep Direction | sed 's/Direction: //g'` 
     sleep 2
 done
