ce09c1bcf6d7711704576759c0c9349ae9d9b987
Matthew Davis <ml.davis195@gmail.com>
2016-12-21 20:06:39 -0500

    Updated atmega.c

--- board/spi/atmega.c
+++ board/spi/atmega.c
@@ -334,13 +334,13 @@ int main(void) {
 		int nums[3] = {0, 0, 0};
 		parse_message(msg, nums);
 
-		if (nums[0] == -255 && nums[1] == -255 && nums[2] == -255) {
-			start_timer();
-		} else if (nums[0] == 0 && nums[1] == 0 && nums[2] == 0) {
-			stop_timer();
-		} else {
-			sendMessage("Unrecognized message received\n");
-		}
+		// if (nums[0] == -255 && nums[1] == -255 && nums[2] == -255) {
+		// 	start_timer();
+		// } else if (nums[0] == 0 && nums[1] == 0 && nums[2] == 0) {
+		// 	stop_timer();
+		// } else {
+		// 	sendMessage("Unrecognized message received\n");
+		// }
 
 		set_wheels(nums[0], nums[1], nums[2]);
     }
