ce8818b177a26f0c107e58fbbfe46010b7b3fce8
Matthew Davis <ml.davis195@gmail.com>
2017-02-06 15:54:14 -0500

    changed initialize pwm function

--- atm.c
+++ atm.c
@@ -133,20 +133,9 @@ void sendInt(int num) {
 // initialize pwm for enablers
 void initialize_pwm() {
 	// TIFR0, TIMSK0, TCCR0B
-	// TCCR0B = (1 << CS00); // no pre-scaling
-	// TCCR0B |= (1 << WGM02) | (1 << WGM01) | (1 << WGM00); // fast pwm / top
-
-	// TCCR0A |= (1 << COM0A1);
-    // set none-inverting mode
-
-	TCCR0A |= (1 << COM0A1);
-    // set none-inverting mode
-
-    TCCR0A |= (1 << WGM01) | (1 << WGM00);
-    // set fast PWM Mode
-
-    TCCR0B |= (1 << CS01);
-    // set prescaler to 8 and starts PWM
+	TCCR0B = (1 << CS00); // no pre-scaling
+	TCCR0B |= (1 << WGM02) | (1 << WGM01) | (1 << WGM00); // fast pwm / top
+	sei();
 }
 
 void init_wheels() {
