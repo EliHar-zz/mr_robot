4c1030b2176e8c25e8bd69c266c0fae25c1604a1
Amir Bawab <amirbawab@gmail.com>
2017-02-19 20:19:47 -0500

    Desgined the nav status

--- dashboard/mr-robot-html/src/app/views/templates/nav/nav-status/nav-status.component.ts
+++ dashboard/mr-robot-html/src/app/views/templates/nav/nav-status/nav-status.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common'
 
 @Component({
   selector: 'mr-nav-status',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavStatusComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _location: Location) { }
 
   ngOnInit() {
   }
 
+  onBackClicked() {
+    this._location.back();
+  }
 }
