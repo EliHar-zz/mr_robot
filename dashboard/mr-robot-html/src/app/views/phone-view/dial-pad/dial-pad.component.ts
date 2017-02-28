8cdee6a3a8d330f70d559574f782b949f960ad90
Amir Bawab <amirbawab@gmail.com>
2017-02-27 19:40:47 -0500

    Added click handler for dial pad

--- dashboard/mr-robot-html/src/app/views/phone-view/dial-pad/dial-pad.component.ts
+++ dashboard/mr-robot-html/src/app/views/phone-view/dial-pad/dial-pad.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialPadComponent implements OnInit {
 
+  public screenValue : string;
   constructor() { }
 
   ngOnInit() {
+    this.screenValue = "";
   }
 
+  public removeLastChar() {
+    if(this.screenValue.length > 0) {
+      this.screenValue = this.screenValue.substring(0,this.screenValue.length-1);
+    }
+  }
+
+  public appendValue(val : string) {
+    this.screenValue += val;
+  }
 }
